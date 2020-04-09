d8a3c42a3e1c05089ecb589055a533d14ca1ab88
shitao25 <44039599+shitao25@users.noreply.github.com>
2020-04-09 19:53:01 -0400

    Add files via upload

---
+++ viewworker.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Table with database</title>
+<style>
+table {
+border-collapse: collapse;
+width: 100%;
+color: #588c7e;
+font-family: monospace;
+font-size: 25px;
+text-align: left;
+}
+th {
+background-color: #988c7e;
+color: white;
+}
+tr:nth-child(even) {background-color: #f2f2f2}
+</style>
+</head>
+<body>
+<table>
+<tr>
+<th>Id</th>
+<th>First Name</th>
+<th>Last Name</th>
+<th>Role</th>
+</tr>
+
+<?php
+//written by Tony Shi
+include_once('db_connect.php');
+$qStr = "SELECT * FROM worker;";
+	$qRes = $db->query($qStr);
+
+	if($qRes != FALSE){
+	while($row = $qRes->fetch())
+	{
+		$id=$row['sid'];
+		$fname=$row['fname'];
+    $lname=$row['lname'];
+    $role=$row['role'];
+		$str="<TR><TD>$id</TD><TD>$fname</TD><TD>$lname</TD><TD>$role</TD></TR>\n";
+		print $str;
+	}
+	}
+?>
+</table>
+</body>
+</html>
